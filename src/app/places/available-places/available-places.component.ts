8ab7e0ff5fd8f18be0c1723868883f2e2eae99f1
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-27 23:02:01 +0100

    Keep the code inside subscribe cleaner and smaller

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import {Place} from '../place.model';
 import {PlacesComponent} from '../places.component';
 import {PlacesContainerComponent} from '../places-container/places-container.component';
 import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs";
+import {catchError, map, throwError} from "rxjs";
 
 @Component({
   selector: 'app-available-places',
@@ -24,16 +24,22 @@ export class AvailablePlacesComponent implements OnInit {
     this.isFetching.set(true);
     const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
-        map(data => data.places)
+        map(data => data.places),
+        catchError((error) => {
+            console.log(error);
+            return throwError(
+              () =>
+                new Error("Something went wrong when fetching!")
+            )
+          }
+        )
       )
       .subscribe({
         next: (places) => {
           this.places.set(places);
         },
-        error: (error) => {
-          // this.error.set(error.message)
-          console.log(error);
-          this.error.set("Something went wrong when fetching!")
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => {//the complete is important to guarantee that the request is done
           this.isFetching.set(false);
