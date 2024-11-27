b0adcdfe43f14adaa3d425f62fb7b25a61356361
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-28 00:28:05 +0100

    Update user places automatically

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -24,9 +24,11 @@ export class PlacesService {
       }))
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    this.userPlaces.update(prevPlaces => [...prevPlaces, place]);//update automatically user places
+
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId
+      placeId: place.id,
     });
   }
 
