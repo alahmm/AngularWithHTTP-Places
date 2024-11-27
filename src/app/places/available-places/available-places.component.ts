4534d8cbe3c9b9fc75d8c7eb3e454891f4351b91
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-27 22:23:19 +0100

    Use signal to trigger the response

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -21,7 +21,7 @@ export class AvailablePlacesComponent implements OnInit {
     const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
       .subscribe({
       next: (data) => {
-        console.log(data.places);
+        this.places.set(data.places);
         // console.log(response.body?.places);
       }
     });
